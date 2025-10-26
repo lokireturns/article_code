4170757b2932f2d2e9e49947740f01fa87b9cd25
lokireturns <chachipereira0@gmail.com>
2025-10-26 11:54:33 +0000

    Complete lru cache

--- lru_cache.go
+++ lru_cache.go
@@ -2,13 +2,12 @@
 
 package main
 
-import "fmt"
-
 // Some page in our buffer pool
 type Node struct {
 	next *Node
 	prev *Node
 	val  string
+	key  int
 }
 
 type LruCache struct {
@@ -37,26 +36,73 @@ func (lc *LruCache) newHead(node *Node, key int) {
 }
 
 func (lc *LruCache) evict() {
-	var penultimateNode *Node = lc.tail.prev
-	lc.tail = penultimateNode
-	lc.tail.next = nil
+	if lc.tail == nil {
+		return // Nothing to evict
+	}
+
+	nodeToRemove := lc.tail
+	delete(lc.index, nodeToRemove.key)
+
+	if lc.tail.prev != nil {
+		// There's more than one node...
+		lc.tail = lc.tail.prev
+		lc.tail.next = nil
+	} else {
+		// Only one node - list becomes empty
+		lc.tail = nil
+		lc.head = nil
+	}
+
+	lc.tail = nodeToRemove.prev
+	lc.currentSize--
 }
 
 func (lc *LruCache) put(key int, val string) {
-	// If key exists the update it and move to MSU
 	targetNode, exists := lc.index[key]
 
 	if exists {
-		// Assuming its not the head or tail
-		fmt.Print("key found: %d", key)
+		targetNode.val = val
+		// Only move to head if not already there
+		if targetNode != lc.head {
+
+			if targetNode.next != nil {
+				targetNode.next.prev = targetNode.prev
+			}
+
+			if targetNode.prev != nil {
+				targetNode.prev.next = targetNode.next
+			}
+
+			if targetNode == lc.tail {
+				lc.tail = targetNode.prev
+			}
+
+			lc.newHead(targetNode, key)
+		}
 	} else {
-		cachedNode := *lc.head
-		newNode := Node{next: &cachedNode, prev: lc.tail, contents: val}
-		cachedNode.prev = &newNode
+
+		if lc.currentSize == lc.capacity {
+			lc.evict()
+		}
+
+		newNode := Node{val: val, key: key}
+		lc.newHead(&newNode, newNode.key)
+		lc.currentSize++
+	}
+
+}
+
+func newCache(capacity int) *LruCache {
+	return &LruCache{
+		capacity: capacity,
+		index:    make(map[int]*Node),
 	}
 
 }
 
 func main() {
-
+	cache := newCache(2)
+	cache.put(35, "foo")
+	cache.put(22, "bar")
+	cache.put(22, "Cheese")
 }
