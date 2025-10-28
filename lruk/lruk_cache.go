a7c4c28ac271745d31888fe4301e83ac53b9ba37
lokireturns <chachipereira0@gmail.com>
2025-10-28 22:28:24 +0000

    progress with lruk

--- lruk/lruk_cache.go
+++ lruk/lruk_cache.go
@@ -14,19 +14,19 @@ import (
 )
 
 type Node struct {
+	next      *Node
+	prev      *Node
 	key       int
-	val       any
 	access    []int
 	evictable bool
 }
 
 // TODO Should we refer to these as frames?
 // Since the value member is where we store the pointer to the actual page?
-func NewNode(key int, k int, val any) *Node {
+func NewNode(key int, k int) *Node {
 	return &Node{
 		key:    key,
 		access: make([]int, k),
-		val:    val,
 	}
 }
 
@@ -38,18 +38,17 @@ func NewNode(key int, k int, val any) *Node {
 // - O(n) eviction scan for simplicity
 // - Capacity managed externally by buffer pool
 type LrukCache struct {
-	index    map[int]*Node
-	history  []*Node
-	capacity int
-	k        int
+	head        *Node
+	tail        *Node
+	index       map[int]*Node
+	k           int
+	currentSize int
 }
 
 func NewLrukCache(capacity int, k int) *LrukCache {
 	return &LrukCache{
-		k:        k,
-		capacity: capacity,
-		index:    make(map[int]*Node, capacity),
-		history:  make([]*Node, capacity),
+		k:     k,
+		index: make(map[int]*Node),
 	}
 }
 
@@ -65,14 +64,15 @@ func (lc *LrukCache) RecordAccess(frame_id int, page *Node) {
 		targetPage.access[len(targetPage.access)-1] = int(time.Now().UnixMilli())
 	} else {
 		page.access[0] = int(time.Now().UnixMilli())
-		lc.history = append(lc.history, page)
-		lc.index[frame_id] = lc.history[0]
+		lc.tail.next = page
+		lc.tail = page
+		lc.index[frame_id] = page
 	}
 }
 
 // Toggle page as evictable or not
 // When page pin count hits 0 page is evictable
-// - Controls the cache capacity value
+// - Controls the cache size value
 // - Assumes pin count is known when called from buffer pool
 func (lc *LrukCache) MarkEvictable(frame_id int, set_evictable bool) (int, error) {
 	targetPage, exists := lc.index[frame_id]
@@ -81,16 +81,53 @@ func (lc *LrukCache) MarkEvictable(frame_id int, set_evictable bool) (int, error
 	}
 	if set_evictable {
 		targetPage.evictable = true
-		lc.capacity++
+		lc.currentSize++
 		return 0, nil
 	}
 
 	targetPage.evictable = false
-	lc.capacity--
+	lc.currentSize--
 	return 0, nil
 }
 
-// Return number of evictable frames currently in the cache
-// func (lc *LrukCache) Size() int {
+// Evict frame with largest backward k-distance
+// among evictable frames.
+func (lc *LrukCache) Evict() int {
+	if lc.currentSize < 1 {
+		return -1
+	}
+	now := time.Now().UnixMilli()
+	var maxkBackwardsDistance int64
+	var evictionFrameId int
 
-// }
+	for i, p := range lc.index {
+		if p.evictable {
+			kBackwardsDistance := now - int64(p.access[(lc.k)-1])
+			if kBackwardsDistance > maxkBackwardsDistance {
+				maxkBackwardsDistance = kBackwardsDistance
+				evictionFrameId = i
+			}
+		}
+	}
+	targetNode := lc.index[evictionFrameId]
+
+	// TODO do we need to nil the next and prev pointers of our
+	// target node to ensure its de allocated by garbage collector?
+	if lc.head == targetNode {
+		lc.head = targetNode.next
+	}
+
+	if lc.tail == targetNode {
+		lc.tail = targetNode.prev
+	}
+
+	targetNode.prev.next = targetNode.next
+	targetNode.next.prev = targetNode.prev
+	lc.currentSize--
+	return evictionFrameId
+}
+
+// Return number of evictable frames currently in the cache
+func (lc *LrukCache) Size() int {
+	return lc.currentSize
+}
